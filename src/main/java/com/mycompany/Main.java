a8bc823a7820f7c2d37b38031c5334ed17fc3346
Juan Diego Sierra Teran <jdiegosierra@unicesar.edu.co>
2026-08-17 13:16:39 -0500

    feat: implementar READ de Libro (listar)

--- src/main/java/com/mycompany/Main.java
+++ src/main/java/com/mycompany/Main.java
@@ -101,4 +101,17 @@ public class Main {
         System.out.println("Libro creado exitosamente.");
     }
 
+
+    public static void listarLibros() {
+        System.out.println("\n--- Lista de Libros ---");
+        if (libros.isEmpty()) {
+            System.out.println("No hay libros registrados.");
+            return;
+        }
+        for (Libro l : libros) {
+            System.out.println(l);
+        }
+    }
+    
+
 }
